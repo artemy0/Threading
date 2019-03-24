0b72be6b26f1724591cb3b55c9a6900813cc0cd5
Artem Gorbachev <gorbachev99artem@gmail.com>
2019-03-24 19:13:22 +0300

    Threading

---
+++ Threading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Threading
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Press any key to start the stopwatch.\n");
+            Console.ReadKey();
+            //Console.Clear();
+
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            Console.ReadKey();
+
+            sw.Stop();
+
+            //Console.Clear();
+            Console.WriteLine("\n\nResult: " + sw.Result); //repeated output of results.
+
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
---
+++ Threading/Stopwatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Stopwatch
+    {
+        private bool Stopped;
+        private int AccuracyInMilliseconds;
+        private Thread t;
+
+        public TimeSpan Result { get; private set; } = TimeSpan.Zero;
+
+        public Stopwatch(int accuracyInMilliseconds = 10)
+        {
+            AccuracyInMilliseconds = accuracyInMilliseconds;
+        }
+
+        public void Start()
+        {
+            Stopped = false;
+
+            //increase time in a separate thread.
+            t = new Thread(new ThreadStart(() =>
+            {
+                Console.WriteLine("Press any button to stop the stopwatch.");
+                //the time when we started counting.
+                DateTime start = DateTime.Now;
+
+                while (!Stopped)
+                {
+                    //disable cursor visibility to remove unwanted blinking
+                    Console.CursorVisible = false;
+
+                    Console.CursorLeft = 0;
+                    Result = DateTime.Now - start; //the time that has passed since we started.
+                    Console.Write(Result);
+
+                    Thread.Sleep(AccuracyInMilliseconds);
+                }
+
+                //return cursor visibility
+                Console.CursorVisible = true;
+            }));
+
+            t.Start();
+        }
+
+        public void Stop()
+        {
+            Stopped = true;
+            t.Join();
+        }
+    }
+}
